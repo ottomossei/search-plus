6994f16669656bd20ffd25e58fb6bbb53b8bee17
ottomossei <seki.jobhunting@gmail.com>
2021-11-07 13:53:26 +0900

    v1.1

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,5 @@
 var form = document.forms.jsform;
-var array_display = ['aim','keyword','addword']
+var array_display = ['aim','keyword','addword','exceptword','obj_site']
 
 function logtime(){
 	var now = new Date();
@@ -23,11 +23,21 @@ $(document).ready( function(){
 $(document).ready( function(){
     chrome.storage.local.get(array_display,function(value){
         if (typeof value.keyword === "undefined") {
-            form.aim.value = "https://www.google.com/search?q=";//初期値はGoogle
+            form.aim.value = "";//初期値はDefault
         }else{
             form.aim.value = value.aim;
         }
-        form.keyword.value = "";//キーワード
+        if (typeof value.exceptword === "undefined") {
+            form.exceptword.value = "";
+        }else{
+            form.exceptword.value = value.exceptword;
+        }
+        if (typeof value.keyword === "undefined") {
+            form.keyword.value = "";//初期値はDefault
+        }else{
+            form.keyword.value = value.keyword;
+        }
+        form.obj_site.value = value.obj_site;//対象サイト
         form.addword.value = value.addword;//アドワード
     })
 });
@@ -35,11 +45,18 @@ $(document).ready( function(){
 //保存
 document.getElementById('jsform').onsubmit = function(){
     var  aim = form.aim.value;//検索サイト
+    var obj_site = form.obj_site.value;//対象サイト
     var  keyword = form.keyword.value;//キーワード
+    if (form.exceptword.value != "") {
+        var exceptword = '+-' + form.exceptword.value;//除外ワード
+    }else{
+        var exceptword = '';//除外ワード
+    }
     var  addword = form.addword.value;//アドワード
-    var array_form = {'aim':aim,'keyword':keyword, 'addword':addword}
+    var array_form = {'aim':aim,'keyword':keyword, 'addword':addword, 'exceptword':exceptword, 'obj_site':obj_site}
     chrome.storage.local.set(array_form);
-    var newurl = aim+addword+'+'+keyword;
+    var url_google = "https://www.google.com/search?q="
+    var newurl = url_google+addword+'+'+keyword+exceptword+obj_site+aim;
     chrome.tabs.query({'active': true}, function(tabs) {
         chrome.tabs.create({url:newurl});
     });
